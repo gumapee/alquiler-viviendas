2987b2af65ca6e4fbb64c13ddaca3c328572cc65
Guillermo Marcos Perez <guillermo.marcos2@alu.uclm.es>
2026-06-17 22:08:26 +0200

    Inicializa proyecto Spring Boot con pantalla principal

---
+++ src/main/java/es/uclm/alquilerviviendas/AlquilerViviendasApplication.java
@@ -0,0 +1,13 @@
+package es.uclm.alquilerviviendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilerViviendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlquilerViviendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/uclm/alquilerviviendas/AlquilerViviendasApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uclm.alquilerviviendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquilerViviendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
